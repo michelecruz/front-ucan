4bd974822bd55524d7000cb67c5f834ba4aa8445
julianacnascimento <juliana.conceicao@dce.ufpb.br>
2019-04-08 15:54:40 -0300

    atualizacao da branch

--- myproject/src/App.vue
+++ myproject/src/App.vue
@@ -1,13 +1,11 @@
 <template>
   <div id="app">
-     <!-- <img src="./assets/ucan.png"> -->
+    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
-
-
 export default {
   name: 'App'
 }
@@ -15,11 +13,11 @@ export default {
 
 <style>
 #app {
-  font-family: 'Corbel', Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
 }
-
 </style>
--- myproject/src/router/index.js
+++ myproject/src/router/index.js
@@ -1,32 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import BCard from 'bootstrap-vue/es/components/card/card'
-import BCardBody from 'bootstrap-vue/es/components/card/card-body'
-import BCardFooter from 'bootstrap-vue/es/components/card/card-footer'
-import BCardHeader from 'bootstrap-vue/es/components/card/card-header'
-import BCardImg from 'bootstrap-vue/es/components/card/card-img'
-import BTable from 'bootstrap-vue/es/components/table/table'
-import Image from 'bootstrap-vue/es/components/image'
-Vue.use(Image)
-Vue.component('b-card', BCard)
-Vue.component('b-card-body', BCardBody)
-Vue.component('b-card-footer', BCardFooter)
-Vue.component('b-card-header', BCardHeader)
-Vue.component('b-card-img', BCardImg)
-Vue.component('b-table', BTable)
-Vue.use(BootstrapVue)
+import HelloWorld from '@/components/HelloWorld'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/home',
-      name: 'Home',
-      component: Home
-    },
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
   ]
 })
